220e08b926b8032977dec4da65eecc0f4d1f434f
Chris <support@totem.training>
2016-05-21 09:33:47 -0400

    Initial Commit

---
+++ StepSlider.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: StepSlider - A better, basic UISlider
+
+import UIKit
+import XCPlayground
+
+let slider = StepSlider(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+
+slider.customTrack = true
+slider.minValue = 0
+slider.maxValue = 4
+slider.steps = 5
+slider.trackColor = UIColor.blueColor()
+slider.stepTickColor = UIColor.lightGrayColor()
+
+XCPlaygroundPage.currentPage.liveView = slider
+
